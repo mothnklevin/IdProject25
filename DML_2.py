88b324eb5b39ef235b336f44efd802e0e1a9d5c1
mothnklevin <klevin2775@gmail.com>
2025-08-16 00:37:00 +0100

    Change random sampling to explicit sampling

--- DML_2.py
+++ DML_2.py
@@ -1,5 +1,6 @@
 from doubleml import DoubleMLData, DoubleMLPLR
 from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
+from sklearn.model_selection import KFold
 
 def run_doubleml_plr_rf(X, D, Y, n_folds=5, seed=60):
     dml_data = DoubleMLData.from_arrays(X, Y, D)
@@ -10,7 +11,15 @@ def run_doubleml_plr_rf(X, D, Y, n_folds=5, seed=60):
         ml_l=RandomForestRegressor(random_state=seed),
         ml_m=RandomForestRegressor(random_state=seed),
         n_folds=n_folds,
+        draw_sample_splitting=False,  # 关闭自动随机抽样
     )
+
+    # 使用固定的可复现 K 折划分
+    kf = KFold(n_splits=n_folds, shuffle=True, random_state=seed) # 显式生成 K 折划分
+    smpls = [(train_idx, test_idx) for train_idx, test_idx in kf.split(X)] # 构建列表
+    model.set_sample_splitting(smpls)  # 显式传入划分
+
+
     model.fit()
     return {
         'theta_hat': model.coef[0],
